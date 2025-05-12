d647d8412301f642de95bc06221bdf424da41335
JosiasAPC <josias.uni.ccp@gmail.com>
2025-05-12 01:25:22 -0300

    correcting developmen and production variables, and vite folder

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,11 +1,10 @@
 // Fatch or Axios
 import axios from "axios";
-import dotenv from "dotenv";
 
-dotenv.config();
-const NODE_ENV = process.env.NODE_ENV;
-
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+const URL =
+  import.meta.env.VITE_ENV === "development"
+    ? "http://localhost:3000/api"
+    : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
